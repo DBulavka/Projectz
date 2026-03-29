12a9c8f6749693760e773b549766855ef777596c
ds.bulavka <ds.bulavka@gmail.com>
2026-03-29 12:14:14 +0300

    remove unused classes

--- backend/src/main/java/com/example/workflow/repository/ProcessDefinitionMetaRepository.java
+++
@@ -1,3 +0,0 @@
-package com.example.workflow.repository;
-
-// Removed in favor of Flowable RepositoryService queries.
--- backend/src/main/java/com/example/workflow/repository/ProcessInstanceMetaRepository.java
+++
@@ -1,3 +0,0 @@
-package com.example.workflow.repository;
-
-// Removed in favor of Flowable RuntimeService queries.
